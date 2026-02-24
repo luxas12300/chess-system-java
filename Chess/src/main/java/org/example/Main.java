26cad62f8e21089783ef3fc3788946ea6f48968a
Lucas Costa <costalucas017@gmail.com>
2026-02-24 11:48:13 -0300

    Starting to implement Board and Piece

---
+++ Chess/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.boardgame.Board;
+import org.example.boardgame.Position;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+      Board board = new Board(8,8);
+    }
+}
