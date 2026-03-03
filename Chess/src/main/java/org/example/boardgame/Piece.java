bd0d1f43f592a18e775cc211705f35fb041abcc9
Lucas Costa <costalucas017@gmail.com>
2026-03-03 15:00:26 -0300

    Possible moves of a piece

--- Chess/src/main/java/org/example/boardgame/Piece.java
+++ Chess/src/main/java/org/example/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package org.example.boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -13,5 +13,23 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position postition){
+        return possibleMoves()[postition.getRow()][postition.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i< mat.length; i++){
+            for(int j = 0; j< mat.length; j++){
+                if(mat[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
